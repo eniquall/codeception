5d18ed4fa102db01b0f8e01b853b593b15a7c2df
eniquall <eniquall@gmail.com>
2016-05-19 17:01:18 +0300

    Added getElement and clickElement methods

--- src/ExtWebDriver.php
+++ src/ExtWebDriver.php
@@ -15,4 +15,29 @@ class ExtWebDriver extends WebDriver
 		$arr = $this->findFields($selector);
 		return reset($arr);
 	}
+
+	/**
+	 * @param $selector
+	 * @return array|mixed
+	 */
+	public function getElement($selector)
+	{
+		$els = $this->_findElements($selector);
+		return $els ? reset($els) : $els;
+	}
+
+	/**
+	 * @param $selector
+	 * @throws \Exception
+	 */
+	public function clickElement($selector)
+	{
+		$el = $this->getElement($selector);
+
+		if ($el) {
+			$el->click();
+		} else {
+			throw new \Exception('Element can\'t be found: "' . $selector . '"');
+		}
+	}
 }
